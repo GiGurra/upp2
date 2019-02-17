cf331ca53044a748290a2e25232728970c6e9665
gigurra <gigurra@gmail.com>
2019-02-17 15:12:18 +0000

    Add natives and testing out jinput

--- build.sbt
+++ build.sbt
@@ -1,14 +1,25 @@
-import Dependencies._
+ThisBuild / scalaVersion      := "2.12.8"
+ThisBuild / version           := "0.1.0-SNAPSHOT"
+ThisBuild / organization      := "com.github.gigurra"
+ThisBuild / organizationName  := "com.github.gigurra"
+ThisBuild / resolvers         += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+ThisBuild / resolvers         += "jitpack" at "https://jitpack.io"
 
-ThisBuild / scalaVersion     := "2.12.8"
-ThisBuild / version          := "0.1.0-SNAPSHOT"
-ThisBuild / organization     := "com.example"
-ThisBuild / organizationName := "example"
 
 lazy val root = (project in file("."))
   .settings(
     name := "upp2",
-    libraryDependencies += scalaTest % Test
+    libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5",
+    libraryDependencies += "com.github.strikerx3" % "jxinput" % "v1.0.0",
+    libraryDependencies += "net.java.jinput" % "jinput" % "2.0.9",
+    libraryDependencies += "net.java.jinput" % "jinput-platform" % "2.0.7",
+    libraryDependencies += "net.java.jinput" % "windows-plugin" % "2.0.9",
+    libraryDependencies += "commons-io" % "commons-io" % "2.6",
+
+      /*libraryDependencies += scalaTest % Test,
+      libraryDependencies += jxinput,
+      libraryDependencies += jinput,
+      libraryDependencies += jinputNatives,*/
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,7 +1,85 @@
 package example
 
+import java.io.{File, FileOutputStream}
+
+import com.github.strikerx3.jxinput.XInputDevice14
+import net.java.games.input.Event
+import org.apache.commons.io.IOUtils
+
+import scala.util.Try
+
 object Hello extends Greeting with App {
-  println(greeting)
+
+  testJInput()
+
+  private def testJInput(): Unit = {
+
+    // Put natives on librarypath
+    val tempRoot = System.getProperty("java.io.tmpdir")
+    val nativesDir = s"$tempRoot/jinput-natives"
+    new File(nativesDir).mkdirs()
+
+    val natives = Seq(
+      "jinput-dx8.dll",
+      "jinput-dx8_64.dll",
+      "jinput-raw.dll",
+      "jinput-raw_64.dll",
+      "jinput-wintab.dll"
+    )
+
+    for (native <- natives) {
+      val inputStream = getClass.getClassLoader.getResourceAsStream(native)
+      try {
+        val nativeBytes = IOUtils.toByteArray(inputStream)
+        val outputStream = new FileOutputStream(s"$nativesDir/$native")
+        try {
+          IOUtils.write(nativeBytes, outputStream)
+        } finally {
+          Try(outputStream.close())
+        }
+      } finally {
+        Try(inputStream.close())
+      }
+    }
+
+    System.setProperty("net.java.games.input.librarypath", nativesDir)
+
+    val event = new Event
+
+    import net.java.games.input.Controller
+    import net.java.games.input.ControllerEnvironment
+
+    val devices = ControllerEnvironment.getDefaultEnvironment.getControllers
+
+    for (device <- devices) {
+      println(s"device: $device")
+      println(s"  device.getName: ${device.getName}")
+      println(s"  device.getType: ${device.getType}")
+      println(s"  device.getPortNumber: ${device.getPortNumber}")
+      println(s"  device.getPortType ${device.getPortType}")
+      println(s"  device.getComponents: ${device.getComponents.toSeq}")
+      println(s"  device.getControllers: ${device.getControllers.toSeq}")
+      println()
+    }
+  }
+
+  private def testJxInput(): Unit = {
+    val devices = XInputDevice14.getAllDevices
+
+    for (device <- devices) {
+      device.poll()
+      println(device)
+      println(device.getPlayerNum)
+      println(device.getCapabilities)
+      println(device.getGamepadCapabilities)
+      println(device.getComponents)
+      val comps = device.getComponents
+      println(comps.getAxes)
+      println(comps.getButtons)
+      println()
+    }
+  }
+
 }
 
 trait Greeting {
